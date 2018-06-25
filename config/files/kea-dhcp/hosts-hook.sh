4f0fb507d60f96ffbfdac675dfc80a46e26b778d
Jacob Weinstock <jakobweinstock@gmail.com>
2018-06-24 21:42:23 -0600

    add reverse lookup file. might not be needed. add kea config files

---
+++ config/files/kea-dhcp/hosts-hook.sh
@@ -0,0 +1,46 @@
+#!/bin/sh
+# This script adds and removes routes in the Linux kernel whenever a DHCP client
+# gets a lease or a lease expires.
+
+# Protocol to use in "ip route"
+PROTO=static
+LOGFILE=/tmp/kea-hook-runscript-debug.log
+
+case "$1" in
+  "lease4_select")
+    if $(grep -q ${KEA_LEASE4_ADDRESS} /app/config/shared/hostsfile/hosts); then
+      pass
+    else
+      cat <<EOF >> /app/config/shared/hostsfile/hosts
+${KEA_LEASE4_ADDRESS}  ${KEA_LEASE4_HOSTNAME}
+EOF
+    fi
+    echo "== $1 ==" >> $LOGFILE
+    date >> $LOGFILE
+    env >> $LOGFILE
+    echo >> $LOGFILE
+    echo >> $LOGFILE
+    ;;
+  "lease4_renew")
+    echo "== $1 ==" >> $LOGFILE
+    date >> $LOGFILE
+    env >> $LOGFILE
+    echo >> $LOGFILE
+    echo >> $LOGFILE
+    ;;
+  "lease4_release"|"lease4_expire")
+    sed -i "/${KEA_LEASE4_ADDRESS}/d" /app/config/shared/hostsfile/hosts
+    echo "== $1 ==" >> $LOGFILE
+    date >> $LOGFILE
+    env >> $LOGFILE
+    echo >> $LOGFILE
+    echo >> $LOGFILE
+    ;;
+  "lease4_decline")
+    echo "== $1 ==" >> $LOGFILE
+    date >> $LOGFILE
+    env >> $LOGFILE
+    echo >> $LOGFILE
+    echo >> $LOGFILE
+    ;;
+esac
