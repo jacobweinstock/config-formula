aaa81150fefabaa70996e3c1e7ce9378028a9e45
Jacob Weinstock <jakobweinstock.gmail.com>
2018-06-24 22:22:43 -0600

    create log function

--- config/files/kea-dhcp/hosts-hook.sh
+++ config/files/kea-dhcp/hosts-hook.sh
@@ -5,6 +5,14 @@
 LOGFILE=/tmp/kea-hook-runscript-debug.log
 HOSTSFILE=/app/config/shared/hostsfile/hosts
 
+log () {
+  echo "== $1 ==" >> $LOGFILE
+  date >> $LOGFILE
+  env >> $LOGFILE
+  echo >> $LOGFILE
+  echo >> $LOGFILE
+}
+
 case "$1" in
   "lease4_select")
     if $(grep -q ${KEA_LEASE4_ADDRESS} $HOSTSFILE); then
@@ -14,32 +22,17 @@ case "$1" in
 ${KEA_LEASE4_ADDRESS}  ${KEA_LEASE4_HOSTNAME}
 EOF
     fi
-    echo "== $1 ==" >> $LOGFILE
-    date >> $LOGFILE
-    env >> $LOGFILE
-    echo >> $LOGFILE
-    echo >> $LOGFILE
+    log $1
     ;;
   "lease4_renew")
     echo "== $1 ==" >> $LOGFILE
-    date >> $LOGFILE
-    env >> $LOGFILE
-    echo >> $LOGFILE
-    echo >> $LOGFILE
+    log $1
     ;;
   "lease4_release"|"lease4_expire")
     sed -i "/${KEA_LEASE4_ADDRESS}/d" $HOSTSFILE
-    echo "== $1 ==" >> $LOGFILE
-    date >> $LOGFILE
-    env >> $LOGFILE
-    echo >> $LOGFILE
-    echo >> $LOGFILE
+    log $1
     ;;
   "lease4_decline")
-    echo "== $1 ==" >> $LOGFILE
-    date >> $LOGFILE
-    env >> $LOGFILE
-    echo >> $LOGFILE
-    echo >> $LOGFILE
+    log $1
     ;;
 esac
